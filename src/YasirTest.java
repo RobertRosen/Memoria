f603c7aeb537e7e8b5a83e96856bf57497641488
Adel Sabanovic <62741023+Adel-Sabanovic@users.noreply.github.com>
2020-04-07 11:30:46 +0200

    Jag vill kumitta

--- src/YasirTest.java
+++ src/YasirTest.java
@@ -2,4 +2,7 @@ public class YasirTest {
     public static void main(String[] args) {
         System.out.println("tester");
     }
+    public static void glassiirrKladdkaka(){
+        System.out.println("Arafat");
+    }
 }
