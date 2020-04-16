1a258edcdc642ccc0ed70c6bc12ae818b50cb8ae
Adel Sabanovic <62741023+Adel-Sabanovic@users.noreply.github.com>
2020-04-16 20:40:06 +0200

    Updated with correct names and comments

--- src/BoardGUI.java
+++ src/BoardGUI.java
@@ -3,22 +3,27 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * This class contains the game board
+ * @author Adel Sabanovic
+ * @version 1.0
+ * @since 2020-04-16
+ */
 public class BoardGUI extends JFrame implements ActionListener {
 
-
-    private JPanel mainPanel = new JPanel();
-    private JPanel panel = new JPanel();
-    private JPanel Mpanel = new JPanel();
-    private JPanel estPanel = new JPanel();
-    private JPanel panelP1 = new JPanel();
-    private JPanel panelP2 = new JPanel();
-    private JPanel infoPanel = new JPanel();
-    private JPanel infoPanel2 = new JPanel();
-    private JPanel scorePanel = new JPanel();
+    private JPanel pnlMain = new JPanel();
+    private JPanel pnlCards = new JPanel();
+    private JPanel pnlMain2 = new JPanel();
+    private JPanel pnlCenter = new JPanel();
+    private JPanel pnlPlayer1 = new JPanel();
+    private JPanel pnlPlayer2 = new JPanel();
+    private JPanel pnlInfo = new JPanel();
+    private JPanel pnlInfo2 = new JPanel();
+    private JPanel pnlScore = new JPanel();
 
     // private JPanel panelBonus = new JPanel();
 
-    private ImageIcon card = new ImageIcon("images/math4.jpg");
+    private ImageIcon iconCard = new ImageIcon("images/math4.jpg");
     private ImageIcon iconPlus = new ImageIcon("images/plus.gif");
     private ImageIcon iconMinus = new ImageIcon("images/minus.gif");
     private ImageIcon iconPlus2 = new ImageIcon("images/plus.gif");
@@ -26,53 +31,53 @@ public class BoardGUI extends JFrame implements ActionListener {
     private ImageIcon iconPi = new ImageIcon("");
     private ImageIcon iconPi2 = new ImageIcon("");
 
-    private ImageIcon bonus = new ImageIcon("images/bonus.jpg");
-    private ImageIcon logo = new ImageIcon();
-    private ImageIcon memoria = new ImageIcon("images/mem2.jpg");
-    private ImageIcon red = new ImageIcon("images/blue.jpg");
-    private ImageIcon mathLogo = new ImageIcon("images/mathLogo.JPG");
+    private ImageIcon iconBonus = new ImageIcon("images/bonus.jpg");
+    private ImageIcon iconEmptyLogo = new ImageIcon();
+    private ImageIcon iconMemoriaLogo = new ImageIcon("images/mem2.jpg");
+    private ImageIcon iconBlueStripe = new ImageIcon("images/blue.jpg");
+    private ImageIcon iconMathLogo = new ImageIcon("images/mathLogo.JPG");
 
-    private JButton btn1 = new JButton(card);
-    private JButton btn2 = new JButton(card);
-    private JButton btn3 = new JButton(card);
-    private JButton btn4 = new JButton(card);
-    private JButton btn5 = new JButton(card);
-    private JButton btn6 = new JButton(card);
-    private JButton btn7 = new JButton(card);
-    private JButton btn8 = new JButton(card);
-    private JButton btn9 = new JButton(card);
-    private JButton btn10 = new JButton(card);
-    private JButton btn11 = new JButton(card);
-    private JButton btn12 = new JButton(card);
-    private JButton btn13 = new JButton(card);
-    private JButton btn14 = new JButton(card);
-    private JButton btn15 = new JButton(card);
-    private JButton btn16 = new JButton(card);
-    private JButton btn17 = new JButton(card);
-    private JButton btn18 = new JButton(card);
-    private JButton btn19 = new JButton(card);
-    private JButton btn20 = new JButton(card);
-    private JButton btn21 = new JButton(card);
-    private JButton btn22 = new JButton(card);
-    private JButton btn23 = new JButton(card);
-    private JButton btn24 = new JButton(card);
+    private JButton btn1 = new JButton(iconCard);
+    private JButton btn2 = new JButton(iconCard);
+    private JButton btn3 = new JButton(iconCard);
+    private JButton btn4 = new JButton(iconCard);
+    private JButton btn5 = new JButton(iconCard);
+    private JButton btn6 = new JButton(iconCard);
+    private JButton btn7 = new JButton(iconCard);
+    private JButton btn8 = new JButton(iconCard);
+    private JButton btn9 = new JButton(iconCard);
+    private JButton btn10 = new JButton(iconCard);
+    private JButton btn11 = new JButton(iconCard);
+    private JButton btn12 = new JButton(iconCard);
+    private JButton btn13 = new JButton(iconCard);
+    private JButton btn14 = new JButton(iconCard);
+    private JButton btn15 = new JButton(iconCard);
+    private JButton btn16 = new JButton(iconCard);
+    private JButton btn17 = new JButton(iconCard);
+    private JButton btn18 = new JButton(iconCard);
+    private JButton btn19 = new JButton(iconCard);
+    private JButton btn20 = new JButton(iconCard);
+    private JButton btn21 = new JButton(iconCard);
+    private JButton btn22 = new JButton(iconCard);
+    private JButton btn23 = new JButton(iconCard);
+    private JButton btn24 = new JButton(iconCard);
 
-    private JButton panelBonus = new JButton(bonus);
-    private JLabel logo1 = new JLabel(logo);
+    private JButton btnBonus = new JButton(iconBonus);
+    private JLabel lblEmptyLogo = new JLabel(iconEmptyLogo);
 
     private ImageIcon[] iconArray = new ImageIcon[6];
 
-    private JLabel label = new JLabel(memoria);
-    private JLabel ghost = new JLabel(red);
-    private JLabel ghost2 = new JLabel(red);
-    private JLabel ghost3 = new JLabel(red);
-    private JLabel ghost4 = new JLabel(red);
-    private JLabel ghost5 = new JLabel();
-    private JLabel ghost6 = new JLabel();
-    private JLabel ghost7 = new JLabel(red);
-    private JLabel ghost8 = new JLabel(red);
-    private JLabel ghost9 = new JLabel(mathLogo);
-    private JLabel ghost10 = new JLabel();
+    private JLabel lblMemoriaLogo = new JLabel(iconMemoriaLogo);
+    private JLabel lblGhost = new JLabel(iconBlueStripe);
+    private JLabel lblGhost2 = new JLabel(iconBlueStripe);
+    private JLabel lblGhost3 = new JLabel(iconBlueStripe);
+    private JLabel lblGhost4 = new JLabel(iconBlueStripe);
+    private JLabel lblGhost5 = new JLabel();
+    private JLabel lblGhost6 = new JLabel();
+    private JLabel lblGhost7 = new JLabel(iconBlueStripe);
+    private JLabel lblGhost8 = new JLabel(iconBlueStripe);
+    private JLabel lblGhost9 = new JLabel(iconMathLogo);
+    private JLabel lblGhost10 = new JLabel();
 
     public BoardGUI() {
 
@@ -80,146 +85,146 @@ public class BoardGUI extends JFrame implements ActionListener {
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        mainPanel.setBackground(Color.WHITE);
-        add(mainPanel);
+        pnlMain.setBackground(Color.WHITE);
+        add(pnlMain);
 
-        mainPanel.add(label);
-        mainPanel.add(ghost9);
-        mainPanel.add(ghost10);
-        ghost9.setPreferredSize(new Dimension(135, 35));
-        ghost10.setPreferredSize(new Dimension(265, 35));
+        pnlMain.add(lblMemoriaLogo);
+        pnlMain.add(lblGhost9);
+        pnlMain.add(lblGhost10);
+        lblGhost9.setPreferredSize(new Dimension(135, 35));
+        lblGhost10.setPreferredSize(new Dimension(265, 35));
         //label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        ghost.setPreferredSize(new Dimension(340, 20));
-        ghost2.setPreferredSize(new Dimension(340, 20));
-        ghost3.setPreferredSize(new Dimension(125, 20));
-        ghost4.setPreferredSize(new Dimension(125, 20));
-        ghost5.setPreferredSize(new Dimension(120, 18));
-        ghost6.setPreferredSize(new Dimension(120, 18));
-        ghost7.setPreferredSize(new Dimension(125, 20));
-        ghost8.setPreferredSize(new Dimension(125, 20));
+        lblGhost.setPreferredSize(new Dimension(340, 20));
+        lblGhost2.setPreferredSize(new Dimension(340, 20));
+        lblGhost3.setPreferredSize(new Dimension(125, 20));
+        lblGhost4.setPreferredSize(new Dimension(125, 20));
+        lblGhost5.setPreferredSize(new Dimension(120, 18));
+        lblGhost6.setPreferredSize(new Dimension(120, 18));
+        lblGhost7.setPreferredSize(new Dimension(125, 20));
+        lblGhost8.setPreferredSize(new Dimension(125, 20));
 
-        mainPanel.add(Mpanel);
-        Mpanel.setPreferredSize(new Dimension(670, 386));
+        pnlMain.add(pnlMain2);
+        pnlMain2.setPreferredSize(new Dimension(670, 386));
         // Mpanel.setBackground(Color.WHITE);
-        label.setPreferredSize(new Dimension(250, 55));
+        lblMemoriaLogo.setPreferredSize(new Dimension(250, 55));
 
         //TODO - Ska kortas ner med en for-loop.
-        mainPanel.add(btn1);
+        pnlMain.add(btn1);
         btn1.setPreferredSize(new Dimension(52, 72));
         btn1.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn1.setBackground(Color.WHITE);
 
-        mainPanel.add(btn2);
+        pnlMain.add(btn2);
         btn2.setPreferredSize(new Dimension(52, 72));
         btn2.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn2.setBackground(Color.WHITE);
 
-        mainPanel.add(btn3);
+        pnlMain.add(btn3);
         btn3.setPreferredSize(new Dimension(52, 72));
         btn3.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn3.setBackground(Color.WHITE);
 
-        mainPanel.add(btn4);
+        pnlMain.add(btn4);
         btn4.setPreferredSize(new Dimension(52, 72));
         btn4.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn4.setBackground(Color.WHITE);
 
-        mainPanel.add(btn5);
+        pnlMain.add(btn5);
         btn5.setPreferredSize(new Dimension(52, 72));
         btn5.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn5.setBackground(Color.WHITE);
 
-        mainPanel.add(btn6);
+        pnlMain.add(btn6);
         btn6.setPreferredSize(new Dimension(52, 72));
         btn6.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn6.setBackground(Color.WHITE);
 
-        mainPanel.add(btn7);
+        pnlMain.add(btn7);
         btn7.setPreferredSize(new Dimension(52, 72));
         btn7.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn7.setBackground(Color.WHITE);
 
-        mainPanel.add(btn8);
+        pnlMain.add(btn8);
         btn8.setPreferredSize(new Dimension(52, 72));
         btn8.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn8.setBackground(Color.WHITE);
 
-        mainPanel.add(btn9);
+        pnlMain.add(btn9);
         btn9.setPreferredSize(new Dimension(52, 72));
         btn9.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn9.setBackground(Color.WHITE);
 
-        mainPanel.add(btn10);
+        pnlMain.add(btn10);
         btn10.setPreferredSize(new Dimension(52, 72));
         btn10.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn10.setBackground(Color.WHITE);
 
-        mainPanel.add(btn11);
+        pnlMain.add(btn11);
         btn11.setPreferredSize(new Dimension(52, 72));
         btn11.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn11.setBackground(Color.WHITE);
 
-        mainPanel.add(btn12);
+        pnlMain.add(btn12);
         btn12.setPreferredSize(new Dimension(52, 72));
         btn12.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn12.setBackground(Color.WHITE);
 
-        mainPanel.add(btn13);
+        pnlMain.add(btn13);
         btn13.setPreferredSize(new Dimension(52, 72));
         btn13.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn13.setBackground(Color.WHITE);
 
-        mainPanel.add(btn14);
+        pnlMain.add(btn14);
         btn14.setPreferredSize(new Dimension(52, 72));
         btn14.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn14.setBackground(Color.WHITE);
 
-        mainPanel.add(btn15);
+        pnlMain.add(btn15);
         btn15.setPreferredSize(new Dimension(52, 72));
         btn15.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn15.setBackground(Color.WHITE);
 
-        mainPanel.add(btn16);
+        pnlMain.add(btn16);
         btn16.setPreferredSize(new Dimension(52, 72));
         btn16.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn16.setBackground(Color.WHITE);
 
-        mainPanel.add(btn17);
+        pnlMain.add(btn17);
         btn17.setPreferredSize(new Dimension(52, 72));
         btn17.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn17.setBackground(Color.WHITE);
 
-        mainPanel.add(btn18);
+        pnlMain.add(btn18);
         btn18.setPreferredSize(new Dimension(52, 72));
         btn18.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn18.setBackground(Color.WHITE);
 
-        mainPanel.add(btn19);
+        pnlMain.add(btn19);
         btn19.setPreferredSize(new Dimension(52, 72));
         btn19.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn19.setBackground(Color.WHITE);
 
-        mainPanel.add(btn20);
+        pnlMain.add(btn20);
         btn20.setPreferredSize(new Dimension(52, 72));
         btn20.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn20.setBackground(Color.WHITE);
 
-        mainPanel.add(btn21);
+        pnlMain.add(btn21);
         btn21.setPreferredSize(new Dimension(52, 72));
         btn21.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn21.setBackground(Color.WHITE);
 
-        mainPanel.add(btn22);
+        pnlMain.add(btn22);
         btn22.setPreferredSize(new Dimension(52, 72));
         btn22.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn22.setBackground(Color.WHITE);
 
-        mainPanel.add(btn23);
+        pnlMain.add(btn23);
         btn23.setPreferredSize(new Dimension(52, 72));
         btn23.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn23.setBackground(Color.WHITE);
 
-        mainPanel.add(btn24);
+        pnlMain.add(btn24);
         btn24.setPreferredSize(new Dimension(52, 72));
         btn24.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         btn24.setBackground(Color.WHITE);
@@ -251,77 +256,77 @@ public class BoardGUI extends JFrame implements ActionListener {
         btn23.addActionListener(this);
         btn24.addActionListener(this);
 
-        Mpanel.add(panel);
+        pnlMain2.add(pnlCards);
         //panel.setBorder(BorderFactory.createTitledBorder(""));
-        panel.add(ghost);
-        panel.setPreferredSize(new Dimension(380, 380));
-        panel.setBackground(Color.WHITE);
+        pnlCards.add(lblGhost);
+        pnlCards.setPreferredSize(new Dimension(380, 380));
+        pnlCards.setBackground(Color.WHITE);
         //panel.add(label);
         // TODO - Ska kortas ner med en for-loop.
-        panel.add(btn1);
-        panel.add(btn2);
-        panel.add(btn3);
-        panel.add(btn4);
-        panel.add(btn5);
-        panel.add(btn6);
-        panel.add(btn7);
-        panel.add(btn8);
-        panel.add(btn9);
-        panel.add(btn10);
-        panel.add(btn11);
-        panel.add(btn12);
-        panel.add(btn13);
-        panel.add(btn14);
-        panel.add(btn15);
-        panel.add(btn16);
-        panel.add(btn17);
-        panel.add(btn18);
-        panel.add(btn19);
-        panel.add(btn20);
-        panel.add(btn21);
-        panel.add(btn22);
-        panel.add(btn23);
-        panel.add(btn24);
-        panel.add(ghost2);
+        pnlCards.add(btn1);
+        pnlCards.add(btn2);
+        pnlCards.add(btn3);
+        pnlCards.add(btn4);
+        pnlCards.add(btn5);
+        pnlCards.add(btn6);
+        pnlCards.add(btn7);
+        pnlCards.add(btn8);
+        pnlCards.add(btn9);
+        pnlCards.add(btn10);
+        pnlCards.add(btn11);
+        pnlCards.add(btn12);
+        pnlCards.add(btn13);
+        pnlCards.add(btn14);
+        pnlCards.add(btn15);
+        pnlCards.add(btn16);
+        pnlCards.add(btn17);
+        pnlCards.add(btn18);
+        pnlCards.add(btn19);
+        pnlCards.add(btn20);
+        pnlCards.add(btn21);
+        pnlCards.add(btn22);
+        pnlCards.add(btn23);
+        pnlCards.add(btn24);
+        pnlCards.add(lblGhost2);
 
-        Mpanel.add(estPanel);
-        estPanel.add(ghost3);
-        estPanel.setPreferredSize(new Dimension(126, 380));
-        estPanel.setBackground(Color.WHITE);
+        pnlMain2.add(pnlCenter);
+        pnlCenter.add(lblGhost3);
+        pnlCenter.setPreferredSize(new Dimension(126, 380));
+        pnlCenter.setBackground(Color.WHITE);
 
-        estPanel.add(panelP1).setPreferredSize(new Dimension(126, 100));
-        estPanel.add(panelP2).setPreferredSize(new Dimension(126, 100));
+        pnlCenter.add(pnlPlayer1).setPreferredSize(new Dimension(126, 100));
+        pnlCenter.add(pnlPlayer2).setPreferredSize(new Dimension(126, 100));
         // estPanel.setBorder(BorderFactory.createTitledBorder(""));
-        estPanel.add(ghost6);
-        estPanel.add(panelBonus).setPreferredSize(new Dimension(126, 50));
+        pnlCenter.add(lblGhost6);
+        pnlCenter.add(btnBonus).setPreferredSize(new Dimension(126, 50));
         // estPanel.add(logo1).setPreferredSize(new Dimension(126,100));
-        panelP1.setBorder(BorderFactory.createTitledBorder("Player one score"));
-        panelP1.setBackground(Color.WHITE);
-        panelP2.setBorder(BorderFactory.createTitledBorder("Player two score"));
-        panelP2.setBackground(Color.WHITE);
-        estPanel.add(ghost5);
-        estPanel.add(ghost4);
+        pnlPlayer1.setBorder(BorderFactory.createTitledBorder("Player one score"));
+        pnlPlayer1.setBackground(Color.WHITE);
+        pnlPlayer2.setBorder(BorderFactory.createTitledBorder("Player two score"));
+        pnlPlayer2.setBackground(Color.WHITE);
+        pnlCenter.add(lblGhost5);
+        pnlCenter.add(lblGhost4);
 
-        Mpanel.add(infoPanel, BorderLayout.EAST);
-        Mpanel.setBackground(Color.WHITE);
-        infoPanel.setPreferredSize(new Dimension(126, 380));
-        infoPanel.add(ghost7);
-        infoPanel.setBackground(Color.WHITE);
-        infoPanel.add(infoPanel2);
-        infoPanel.add(scorePanel);
-        infoPanel.add(logo1).setPreferredSize(new Dimension(126, 96));
-        infoPanel.add(ghost8);
-        infoPanel2.setPreferredSize(new Dimension(126, 100));
-        scorePanel.setPreferredSize(new Dimension(126, 100));
-        infoPanel2.setBorder(BorderFactory.createTitledBorder("Info"));
-        infoPanel2.setBackground(Color.WHITE);
-        scorePanel.setBorder(BorderFactory.createTitledBorder("Highscore"));
-        scorePanel.setBackground(Color.WHITE);
+        pnlMain2.add(pnlInfo, BorderLayout.EAST);
+        pnlMain2.setBackground(Color.WHITE);
+        pnlInfo.setPreferredSize(new Dimension(126, 380));
+        pnlInfo.add(lblGhost7);
+        pnlInfo.setBackground(Color.WHITE);
+        pnlInfo.add(pnlInfo2);
+        pnlInfo.add(pnlScore);
+        pnlInfo.add(lblEmptyLogo).setPreferredSize(new Dimension(126, 96));
+        pnlInfo.add(lblGhost8);
+        pnlInfo2.setPreferredSize(new Dimension(126, 100));
+        pnlScore.setPreferredSize(new Dimension(126, 100));
+        pnlInfo2.setBorder(BorderFactory.createTitledBorder("Info"));
+        pnlInfo2.setBackground(Color.WHITE);
+        pnlScore.setBorder(BorderFactory.createTitledBorder("Highscore"));
+        pnlScore.setBackground(Color.WHITE);
 
         // estPanel.setBorder(BorderFactory.createTitledBorder("Score"));
         // estPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
-        this.iconArray = randomArray();
+        this.iconArray = randomIconArray();
 
     }
 
@@ -367,7 +372,12 @@ public class BoardGUI extends JFrame implements ActionListener {
         }
     }
 
-    private ImageIcon[] randomArray() {
+    /**
+     * Shuffles the player cards randomly on the player board
+     * https://stackoverflow.com/questions/47577638/create-a-memory-game-using-jbuttons
+     */
+
+    private ImageIcon[] randomIconArray() {
         iconArray[0] = iconPlus;
         iconArray[1] = iconMinus;
         iconArray[2] = iconPlus2;
