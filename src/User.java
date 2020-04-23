e2a12dbfdd037655f9cc97a56a6afc61c98e01a0
JoakimTell <62691952+JoakimTell@users.noreply.github.com>
2020-04-23 14:11:11 +0200

    Skapat en Userklass.

---
+++ src/User.java
@@ -0,0 +1,41 @@
+public class User {
+
+    private String userName;
+    private int totalPoints;
+    private int gameScore;
+
+    public User(String userName, int totalPoints) {
+        this.totalPoints = totalPoints;
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getGameScore() {
+        return gameScore;
+    }
+
+    public void setGameScore(int gameScore) {
+        this.gameScore = gameScore;
+    }
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public void setTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
+
+    /*
+    Som en användare behöver jag ett användarnamn, poäng under tiden jag spelar en match, totala poäng.
+    set och get metoder
+    konstruktor
+     */
+}
